157dab23040930923f97fe456b304ef751e291c9
Luca Ban <mesqueeb@users.noreply.github.com>
2026-03-20 14:54:48 +1300

    lint: set up lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,22 @@
 import config from '@cycraft/eslint/config'
+import obsidianmd from 'eslint-plugin-obsidianmd'
+import tseslint from 'typescript-eslint'
+import globals from 'globals'
 
-export default config
+export default [
+  { ignores: ['node_modules/', 'main.js'] },
+  ...config,
+  ...tseslint.config(...obsidianmd.configs.recommended),
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.es2020,
+      },
+      parserOptions: {
+        project: './tsconfig.eslint.json',
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+]
